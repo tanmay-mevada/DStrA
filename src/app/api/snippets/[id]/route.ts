c1442bb6deb46d57b103c1d7973a448169dac00d
tanmay-mevada <tanmaymevada24@gmail.com>
2025-07-09 21:12:11 +0530

    stylingg0

--- src/app/api/snippets/[id]/route.ts
+++ src/app/api/snippets/[id]/route.ts
@@ -2,7 +2,7 @@ import connectDB from '@/lib/db';
 import { Snippet } from '@/models/snippet';
 import { NextRequest, NextResponse } from 'next/server';
 
-export async function DELETE(_: NextRequest, context: { params: { id: string } }) {
+export async function DELETE(_: NextRequest, context) {
   await connectDB();
   const { id } = context.params;
   await Snippet.findByIdAndDelete(id);
