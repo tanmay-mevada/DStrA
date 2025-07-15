72ae54364bc31154e735171092d04a36f309c9a1
tanmay-mevada <tanmaymevada24@gmail.com>
2025-07-15 23:19:58 +0530

    verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import { ThemeProvider } from '@/components/ThemeProvider';
 import AuthProvider from '@/components/AuthProvider';
 import Navbar from '@/components/Navbar';
 import { Toaster } from 'react-hot-toast';
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -16,6 +18,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             <Navbar />
             <Toaster position="top-center" />
             {children}
+            <Analytics />
+            <SpeedInsights />
           </AuthProvider>
         </ThemeProvider>
       </body>
